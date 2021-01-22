5aa445b88ce6b8894f5aacee1c425d7a0f2673eb
KEIHIROZANE <orfis_398398@icloud.com>
2021-01-22 10:12:23 +0000

    [Finish]4-3

---
+++ meshiterro/config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = 'ae3f208c4cdea1abde53c433f714263724c6db5333270ccf531992d845b0052c6e94962b1ac27f639e43cead97570410eafbd5368b817849724121ce1042e155'
